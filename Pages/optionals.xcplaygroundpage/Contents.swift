6f98e8b71354bb1941e23bd9866de548a22850bc
memorycancel <memorycancel@gmail.com>
2023-03-13 11:46:14 +0800

    Modify optionals section

--- Pages/optionals.xcplaygroundpage/Contents.swift
+++ Pages/optionals.xcplaygroundpage/Contents.swift
@@ -1,7 +1,27 @@
-//: [Previous](@previous)
+var ages: [Int] = [3,4,1,2]
+ages.sort()
 
-import Foundation
+// if let
+if let oldestAge = ages.last {
+    print("The oldest age is \(oldestAge).")
+} else {
+    print("There is no oldest age. You must have no students.")
+}
 
-var greeting = "Hello, playground"
+// nil coalescing
+//let oldestAge = ages.last ?? 999
 
-//: [Next](@next)
+// guard statement
+func getOldestAge() {
+    guard let oldestAge = ages.last else {
+        return
+    }
+    
+    print("\(oldestAge) is the oldest age.")
+    // a lot of code
+}
+
+getOldestAge()
+
+// force unwrap
+let oldestAge = ages.last!
